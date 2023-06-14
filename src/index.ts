f7ce92be192e5c7f9c61856e2b40862df4820c34
Iago Becker <iago.mendonca20@gmail.com>
2023-06-14 16:08:15 -0300

    feat: add express initial server

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
-console.log("hello world");
- 
+import express from "express";
+import { config } from "dotenv";
+
+config();
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`lintening on port ${port}`));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
