4e8cf88e9b35a3ceb604f70d524bb1ca866c3d57
Iago Becker <iago.mendonca20@gmail.com>
2023-06-16 10:06:00 -0300

    feat: getUsers-Controller

--- src/controllers/get-users/get-users.ts
+++ src/controllers/get-users/get-users.ts
@@ -1,14 +1,12 @@
 import { IGetUsersController, IGetUsersRepository } from "./protocols";
 
 export class GetUsersController implements IGetUsersController {
-  getUsersRepository: IGetUsersRepository;
-  constructor(getUsersRepository: IGetUsersRepository) {
-    this.getUsersRepository = getUsersRepository;
-  }
-  async handler() {
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
     try {
       // validar requisição
-      // direcionar chamada para o repositori
+      // direcionar chamada para o repositório
       const users = await this.getUsersRepository.getUsers();
 
       return {
