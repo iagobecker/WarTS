0c3ce188c5d6cd011ef4dfd42714de53d3af0c99
Iago Becker <iago.mendonca20@gmail.com>
2023-06-22 20:00:33 -0300

    feat: add CreateUserController with validations

---
+++ src/controllers/create-user/create-user.ts
@@ -0,0 +1,56 @@
+import validator from "validator";
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "./../protocols";
+import {
+  CreateUserParams,
+  ICreateUserController,
+  ICreateUserRepository,
+} from "./protocols";
+
+export class CreateUserController implements ICreateUserController {
+  constructor(private readonly createUserRepository: ICreateUserRepository) {}
+
+  async handle(
+    httpRequest: HttpRequest<CreateUserParams>
+  ): Promise<HttpResponse<User>> {
+    try {
+      //verificar se campos obrigatórios estão presentes
+      const requiredFields = ["name", "email", "password"];
+
+      for (const field of requiredFields) {
+        if (!httpRequest?.body?.[field as keyof CreateUserParams]?.length) {
+          //validar se o body existe
+          return {
+            statusCode: 400,
+            body: `campo ${field} obrigatório`,
+          };
+        }
+      }
+
+      //verificar se  o E-mail é válido
+      const emailIsValid = validator.isEmail(httpRequest.body!.email);
+
+      //se o email não for válido vai executar esse IF
+      if (!emailIsValid) {
+        return {
+          statusCode: 400,
+          body: "E-Mail inválido!",
+        };
+      }
+
+      const user = await this.createUserRepository.createUser(
+        httpRequest.body!
+      );
+
+      return {
+        statusCode: 201,
+        body: user,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Algo deu errado!!",
+      };
+    }
+  }
+}
--- src/repositories/create-user/mongo-create-user.ts
+++ src/repositories/create-user/mongo-create-user.ts
@@ -5,22 +5,23 @@ import {
 import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
-export class MongoCreateUser implements ICreateUserRepository {
+export class MongoCreateUserRepository implements ICreateUserRepository {
   async createUser(params: CreateUserParams): Promise<User> {
+    //criando usuário
     const { insertedId } = await MongoClient.db
       .collection("users")
       .insertOne(params);
-
+    //buscando usuário e verificando se foi criado
     const user = await MongoClient.db
       .collection<Omit<User, "id">>("users")
       .findOne({ _id: insertedId });
-
+    //erro caso não tenha sido criado/encontrado
     if (!user) {
       throw new Error("User não foi criado");
     }
 
     const { _id, ...rest } = user;
-
+    //retorna o usuário criado e substitui o id com _(anderline) por id sem _(anderline)
     return { id: _id.toHexString(), ...rest };
   }
 }
